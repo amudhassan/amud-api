91455ea9fcc33e662e5101b9a4e09ee7c0de8cde
ABDULGHANIYU HASSAN <amudhassan1000@gmail.com>
2026-07-25 01:35:06 +0530

    feat: implement production-grade logs limit security

---
+++ middleware/rateLimiter.js
@@ -0,0 +1,21 @@
+const rateLimit = require("express-rate-limit");
+
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+
+    max: 5,
+
+    standardHeaders: true,
+
+    legacyHeaders: false,
+
+    message: {
+        success: false,
+        message:
+            "Too many requests. Please try again after 15 minutes."
+    }
+});
+
+module.exports = {
+    authLimiter
+};
